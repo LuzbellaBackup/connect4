d55e0326fbcd14d6100309d44a3ce03706a95c0a
LuzbellaBackup <luzbella@mailfence.com>
2024-05-14 18:56:14 -0400

    feat: added player class (I/O reg, User Data)

--- lib/player.rb
+++ lib/player.rb
@@ -1,1 +1,49 @@
 # frozen_string_literal: true
+
+require 'fileutils'
+require 'json'
+
+# Player Class
+class Player
+  attr_reader :name_tag, :symbol, :stats
+
+  def initialize(name_tag, order)
+    @name_tag = name_tag
+    @symbol = order.zero? ? 'a' : 'b'
+    @stats = if File.exist?("data/user/#{@name_tag}.json")
+               read_from_json
+             else
+               default_stats
+             end
+  end
+
+  # FileUtils.mkdir_p 'data/lang' unless Dir.exist? 'data/lang'
+  def default_stats
+    { 'games_played' => 0, 'won' => 0, 'lost' => 0, 'winrate' => 0 }
+  end
+
+  def load_to_json
+    FileUtils.mkdir_p 'data/user' unless Dir.exist? 'data/user'
+    filename = "#{@name_tag}.json"
+    File.open("data/user/#{filename}", 'w') { |file| file.write json_parser }
+    puts "data saved at \"data/user/#{filename}\""
+  end
+
+  def read_from_json
+    filename = "#{@name_tag}.json"
+    file = File.read("data/user/#{filename}")
+    JSON.parse(file)
+  end
+
+  def json_parser
+    to_parse = @stats
+    opts = {
+      array_nl: "\n",
+      object_nl: "\n",
+      indent: '  ',
+      space_before: ' ',
+      space: ' '
+    }
+    JSON.generate(to_parse, opts)
+  end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,4 @@
 # frozen_string_literal: true
+
+## i first thought of developing its test but 99.99% of it is literally just file
+## I/O
